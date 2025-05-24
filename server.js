15340545e1c7b1900a2540a3c3224e15e6a0e873
bruninlins <brunotorresoioizx@gmail.com>
2025-05-24 17:47:27 -0400

    adicionando o express

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
-/* biblioteca para o rodar o servidor sem precisar derrubar */
-const rotas = {
-  "/": "curso de Express. API",
-  "/livros": "Entrei na rota livros",
-  "/autores": "Entrei na rota autores",
-};
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
